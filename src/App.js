30a5389dff3712d9aecb31169f5415f6c48b948d
satyam <styam.7@gmail.com>
2022-08-25 16:01:44 +0530

    new shopping cart

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
 import React, { useEffect, useReducer } from 'react'
 import axios from 'axios'
-import Product from './components/Product'
-import Cart from './components/Cart'
-import reducer from './reducers/reducer'
 
 const App = () => {
 
-  const [state, dispatch] = useReducer(reducer, {
-    products: [],
-    cart: []
-  })
 
-  const fetchProduct = async () => {
-    const { data } = await axios.get('https://fakestoreapi.com/products')
-    dispatch({
-      type: "ADD_PRODUCTS",
-      payload: data
-    })
-  }
-
-  useEffect(() => {
-    fetchProduct()
-  }, [])
 
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'space-between',
-      width: '100%'
-    }}>
-      <Product state={state} dispatch={dispatch}/>
-      <Cart state={state} dispatch={dispatch}/>
-    </div>
+    <>
+     hello
+    </>
   )
 }
 
